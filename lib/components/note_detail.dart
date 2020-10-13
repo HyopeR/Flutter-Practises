23451bd8faa4bfdcc8a651e352560068bc72d66a
HyopeR <tol_celik@hotmail.com>
2020-10-13 12:20:35 +0300

    note_detail component design finish

--- lib/components/note_detail.dart
+++ lib/components/note_detail.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:notebook_app/models/category.dart';
 import 'package:notebook_app/utils/database_helper.dart';
@@ -16,6 +17,7 @@ class _NoteDetailPageState extends State<NoteDetailPage> {
 
   var formKey = GlobalKey<FormState>();
   int selectedCategoryId = 1;
+  double sliderValue = 1.0;
 
   List<Category> categories;
   bool categoriesController = false;
@@ -29,37 +31,245 @@ class _NoteDetailPageState extends State<NoteDetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomPadding: false,
       appBar: AppBar(
         title: Text(widget.title),
         centerTitle: true,
       ),
       body: Container(
         padding: EdgeInsets.all(10),
-        child: Form(
-          key: formKey,
-          child: Column(
-            children: [
-              categoriesController
-                  ? Container(
-                      width: MediaQuery.of(context).size.width,
-                      padding: EdgeInsets.all(3),
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                              color: Theme.of(context).primaryColor, width: 2),
-                          borderRadius: BorderRadius.circular(10)),
-                      child: DropdownButton<int>(
-                        onChanged: (valueId) =>
-                            setState(() => selectedCategoryId = valueId),
-                        items: createDropDownItem(),
-                        value: selectedCategoryId,
-                        underline: Container(),
+        child: ListView(
+          children: [
+
+            Form(
+              key: formKey,
+              child: Column(
+                children: [
+
+                  // Kategori Seçme Row
+                  titleWidget('Not Ekleme'),
+                  Container(
+                      height: 50,
+                      margin: EdgeInsets.symmetric(vertical: 10),
+                      child: Row(
+                        children: [
+
+                          Expanded(
+                            flex: 1,
+                            child: Text('Kategori Seçiniz: ',
+                                style: TextStyle(fontSize: 18)),
+                          ),
+
+                          categoriesController
+                              ? Expanded(
+                            flex: 1,
+                            child: Container(
+                              padding: EdgeInsets.all(3),
+                              decoration: BoxDecoration(
+                                  border: Border.all(
+                                      color: Colors.grey.shade400,
+                                      width: 2),
+                                  borderRadius: BorderRadius.circular(10)),
+                              child: DropdownButton<int>(
+                                isExpanded: true,
+                                onChanged: (valueId) =>
+                                    setState(
+                                            () => selectedCategoryId = valueId),
+                                items: createDropDownItem(),
+                                value: selectedCategoryId,
+                                underline: Container(),
+                              ),
+                            ),
+                          )
+                              : Center(
+                            child: CircularProgressIndicator(),
+                          ),
+                        ],
+                      )
+                  ),
+
+                  // Başlık Input
+                  Container(
+                    height: 50,
+                    margin: EdgeInsets.symmetric(vertical: 10),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'Başlık Giriniz',
+                        labelText: 'Başlık',
+                        labelStyle: TextStyle(fontSize: 18),
+                        border: OutlineInputBorder(),
+
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Theme
+                              .of(context)
+                              .primaryColor, width: 2),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Colors.grey.shade400, width: 2),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
                       ),
-                    )
-                  : Center(
-                      child: CircularProgressIndicator(),
-                    )
-            ],
-          ),
+                    ),
+                  ),
+
+                  // Content Input
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 10),
+                    child: TextFormField(
+                      textAlignVertical: TextAlignVertical.top,
+                      maxLines: 4,
+
+                      decoration: InputDecoration(
+                        alignLabelWithHint: true,
+                        hintText: 'İçerik Giriniz',
+                        labelText: 'İçerik',
+                        labelStyle: TextStyle(fontSize: 18, ),
+                        border: OutlineInputBorder(),
+
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Theme
+                              .of(context)
+                              .primaryColor, width: 2),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Colors.grey.shade400, width: 2),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  // Slider Input
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 10),
+
+                    padding: EdgeInsets.all(3),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                            color: Colors.grey.shade400,
+                            width: 2),
+                        borderRadius: BorderRadius.circular(10)),
+
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+
+                        Padding(
+                          padding: EdgeInsets.all(5),
+                          child: Text('Not Önceliği ($sliderValue)',
+                              style: TextStyle(fontSize: 18)),
+                        ),
+
+                        Container(
+                          child: Flex(
+                            direction: Axis.horizontal,
+                            children: [
+                              Expanded(
+                                flex: 1,
+                                child: MaterialButton(
+                                  padding: EdgeInsets.all(0),
+                                  onPressed: () => setState(() => sliderValue = 1),
+                                  child: Icon(Icons.skip_previous),
+                                ),
+                              ),
+                              Expanded(
+                                flex: 1,
+                                child: MaterialButton(
+                                  padding: EdgeInsets.all(0),
+                                  onPressed: () =>
+                                      setState(() =>
+                                      sliderValue > 1
+                                          ? sliderValue--
+                                          : null),
+                                  child: Icon(Icons.remove),
+                                ),
+                              ),
+                              Expanded(
+                                flex: 5,
+                                child: Slider(
+                                  value: sliderValue,
+                                  onChanged: (value) =>
+                                      setState(() => sliderValue = value),
+
+                                  min: 1.0,
+                                  max: 3.0,
+                                  divisions: 2,
+                                  label: sliderValue.toString(),
+                                ),
+                              ),
+                              Expanded(
+                                flex: 1,
+                                child: MaterialButton(
+                                  padding: EdgeInsets.all(0),
+                                  onPressed: () =>
+                                      setState(() =>
+                                      sliderValue < 3
+                                          ? sliderValue++
+                                          : null),
+                                  child: Icon(Icons.add),
+                                ),
+                              ),
+                              Expanded(
+                                flex: 1,
+                                child: MaterialButton(
+                                  padding: EdgeInsets.all(0),
+                                  onPressed: () => setState(() => sliderValue = 3),
+                                  child: Center(child: Icon(Icons.skip_next)),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+
+                        Center(child: Text('1 Düşük - 2 Orta - 3 Yüksek')),
+                      ],
+                    ),
+                  ),
+
+                  // Button Area
+                  Container(
+                    child: Row(
+
+                      children: [
+
+                        Expanded(
+                          flex: 1,
+                          child: Container(
+                            padding: EdgeInsets.all(10),
+                            child: MaterialButton(
+                              color: Theme.of(context).accentColor,
+                              onPressed: () {},
+                              child: Text('Vazgeç', style: TextStyle(color: Colors.white),),
+                            ),
+                          ),
+                        ),
+
+                        Expanded(
+                          flex: 1,
+                          child: Container(
+                            padding: EdgeInsets.all(10),
+                            child: MaterialButton(
+                              color: Theme.of(context).accentColor,
+                              onPressed: () {},
+                              child: Text('Kaydet', style: TextStyle(color: Colors.white),),
+                            ),
+                          ),
+                        )
+
+                      ],
+
+                    ),
+                  )
+
+                ],
+              ),
+            )
+
+          ],
         ),
       ),
     );
@@ -67,12 +277,11 @@ class _NoteDetailPageState extends State<NoteDetailPage> {
 
   List<DropdownMenuItem<int>> createDropDownItem() {
     return categories
-        .map((category) => DropdownMenuItem<int>(
-              value: category.categoryId,
-              child: Container(
-                  child: Text(category.categoryTitle)
-              ),
-            ))
+        .map((category) =>
+        DropdownMenuItem<int>(
+          value: category.categoryId,
+          child: Container(child: Text(category.categoryTitle)),
+        ))
         .toList();
   }
 
@@ -83,4 +292,32 @@ class _NoteDetailPageState extends State<NoteDetailPage> {
       categoriesController = true;
     });
   }
+
+  Widget titleWidget(String title) =>
+      Container(
+        margin: EdgeInsets.symmetric(vertical: 5),
+        padding: EdgeInsets.symmetric(vertical: 10),
+        child: Container(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Icon(
+                    Icons.brightness_1,
+                    size: 15,
+                    color: Theme
+                        .of(context)
+                        .primaryColor,
+                  ),
+                  SizedBox(width: 5),
+                  Text(title,
+                      style:
+                      TextStyle(fontSize: 22, fontWeight: FontWeight.bold))
+                ],
+              ),
+              Divider(thickness: 2)
+            ],
+          ),
+        ),
+      );
 }
